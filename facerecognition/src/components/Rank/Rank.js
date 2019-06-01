67966f86a32df7a1b736e750817ec517e3dd7754
carlacarr37 <carlacarr37@gmail.com>
2019-06-01 12:15:38 -0500

    Uploading facerecognition app

---
+++ facerecognition/src/components/Rank/Rank.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f4'>
+                {'Mr. Bennett, your current rank is...'}
+            </div>    
+            <div className='white f1'>
+                {'#5'}
+            </div>      
+        </div>
+
+    );
+
+
+}
+
+export default Rank;
