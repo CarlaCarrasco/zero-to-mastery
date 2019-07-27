58a7ada6a7af4ba7b168280faabeee5babd5491c
carlacarr37 <carlacarr37@gmail.com>
2019-07-27 11:11:10 -0500

    Add user name

--- facerecognition/src/components/Rank/Rank.js
+++ facerecognition/src/components/Rank/Rank.js
@@ -5,7 +5,7 @@ const Rank = ({name, entries}) => {
     return (
         <div>
             <div className='white f4'>
-                {name}, your current rank is...
+                {name}, your current rank is...3
             </div>    
             <div className='white f1'>
                 #{entries}
