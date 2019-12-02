37bd6064d1d0eb378df11d9ddf5c9639d0058b29
CarlaCarrasco <49591925+CarlaCarrasco@users.noreply.github.com>
2019-12-02 07:15:42 -0600

    Add files via upload

---
+++ xmas-challenge/day2.js
@@ -0,0 +1,13 @@
+// 1. 01 - first position
+// 2. 02 - second position
+// 3. 03 - output
+// 3. Get value of position[1]
+// 5. Get value of position[2]
+// 6. Add position[1] + position[2]
+// 7. Insert value into position[3].value 
+// NEXT.. 
+// 1. Step foreward 4 positions from where you started position[1] - meaning position[4]
+// 2. From here the new positions remain as [0,1,2,3] 
+// 3. Grab position[1] and position[2]
+// 4. multiply position[1] * position[2]
+// 5. Store value in position[3].value
